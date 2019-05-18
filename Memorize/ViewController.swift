0043d74e7e0933fc81116a5c3f1413610aa38ee5
Bram Anwar <bramanwr@icloud.com>
2019-05-18 11:16:37 +0700

    Initial Commit

---
+++ Memorize/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memorize
+//
+//  Created by Bram Anwar on 18/05/19.
+//  Copyright © 2019 Bram Anwar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
